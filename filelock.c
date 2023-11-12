e51969a19c7a1afab54a8b4fccecb1e7374a7467
jbg <kjbg4565388@gmail.com>
2023-11-12 19:18:09 +0900

    lab3 directory

---
+++ filelock.c
@@ -0,0 +1,52 @@
+/**
+lab3 - 2.8
+*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	int fd;	
+	struct flock testlock;
+	int pid;
+
+	testlock.l_type = F_WRLCK;
+	testlock.l_whence = SEEK_SET;
+	testlock.l_start = 0;
+	testlock.l_len = 10;
+
+	/* open file */
+	fd = open ("testlock", O_RDWR | O_CREAT, 0666);
+	
+	if(fcntl (fd, F_SETLKW, &testlock) == -1) {
+		perror("parent : locking");
+		exit(1);
+	}
+
+	printf("parent: locked record\n");
+
+	pid = fork();
+
+	/*child process */
+	if (pid == 0) {
+		testlock.l_len = 5;
+
+		if(fcntl(fd, F_SETLKW, &testlock) == -1) {
+			printf("child : locking\n");
+			exit(1);
+		}
+		printf("child : locked\n");
+		sleep(5);
+		printf("child : exiting\n");
+	}
+	else if(pid > 0 ) {
+		sleep(5);
+		printf("parent : exiting\n");
+	}
+	else
+	{
+		perror("fork failed");
+	}
+}
---
+++ process_fork.c
@@ -0,0 +1,28 @@
+/**
+lab3 - 2.5
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	int i;
+	int num;
+
+	if(argc < 1)
+	{
+		printf("hello\n");
+	}
+	else
+	{
+		num = atoi(argv[1]);
+		printf("hello");
+
+		for(i=0; i<num; i++)
+			fork();
+
+		printf("\n");
+	}
+	return 0;
+}
